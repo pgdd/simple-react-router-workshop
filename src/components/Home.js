3785d598b297d95a96635ea8505c0ed2805f10eb
Pierre <pierregiddio@gmail.com>
2021-04-26 17:16:07 +0200

    suport class objects es6 with react

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,26 @@
+const Greeting = ({ name, age }) => {
+  return <h1>Hello {name} {age} </h1>
+}
+
 const Home = (props) => {
+
+    const persons = [
+      {
+        name: "Gege", 
+        age: 23
+      }, 
+      {
+        name: "Pierre", 
+        age: 32
+      }
+    ];
+    const person = 'Gege'
     return(
+    <>
       <h1>Home</h1>
+      {persons.map((person) => <Greeting {...person}/> )}
+    </>
+
     )
 }
 
